0237ba649d9b2c0869bdc7d228026e1e4f931c56
David Morisset <dmorisset@gmail.com>
2015-11-16 20:48:15 +0000

    Adds a condition for the max number of buttons

--- multibrowse.js
+++ multibrowse.js
@@ -3,9 +3,12 @@
 	// Puts an event handler on every input of type file inside an element of class "multibrowse"
 	$(".multibrowse").delegate("input:file", 'change', function() {
 		
-		if ($( ".multibrowse input:file:last" ).val()) {
-			var numberOfFiles = $( ".multibrowse input:file" ).length;
-			var attributes = $( ".multibrowse input:file:first" ).prop( "attributes" );
+		var $filesObject = $(".multibrowse input:file");
+		var numberOfInputs = $filesObject.length +1 ;
+
+		if ( ($filesObject.filter(":last").val()) && (numberOfInputs <= 3) ) {
+			console.log( "NumberOfFiles is " + numberOfFiles );
+			var attributes = $filesObject.filter(":first").prop( "attributes" );
 			var $newFile = $("<input>");
 			$.each(attributes, function() {
 				if ( this.name == "id" || this.name == "name" ) {
@@ -15,7 +18,7 @@
 	    		}
 			});
 
-			$( ".multibrowse input:file:last" ).after( $newFile );
+			$filesObject.filter(":last").after( $newFile );
 		}
 	});
 
