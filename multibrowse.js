e7f6e1857bf8715921d13d132e24f06887ff8e0d
David Morisset <dmorisset@gmail.com>
2015-11-16 18:21:46 +0000

    Adds a new input of type file when a file is selected from the browser

--- multibrowse.js
+++ multibrowse.js
@@ -1,3 +1,9 @@
 (function( $ ) {
 
+	// Puts an event handler on every input of type file inside an element of class "multibrowse"
+	$(".multibrowse").delegate("input:file", 'change', function() {
+		var $newFile = $("<input type=\"file\">");
+		$( ".multibrowse input:file:last" ).after( $newFile );
+	});
+
 }(jQuery));
