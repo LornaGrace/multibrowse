5bf6fef43aca013e8efd713879f7d2aaa001290a
David Morisset <dmorisset@gmail.com>
2015-11-16 20:35:07 +0000

    Adds a validation condition on the last input that checks if it is empty or not. If it is, then no other button is created

--- multibrowse.js
+++ multibrowse.js
@@ -2,19 +2,21 @@
 
 	// Puts an event handler on every input of type file inside an element of class "multibrowse"
 	$(".multibrowse").delegate("input:file", 'change', function() {
-		var $newFile = $("<input>");
+		
+		if ($( ".multibrowse input:file:last" ).val()) {
+			var numberOfFiles = $( ".multibrowse input:file" ).length;
+			var attributes = $( ".multibrowse input:file:first" ).prop( "attributes" );
+			var $newFile = $("<input>");
+			$.each(attributes, function() {
+				if ( this.name == "id" || this.name == "name" ) {
+					$newFile.attr(this.name, (this.value) + ( numberOfFiles + 1 ));
+				} else {
+	    			$newFile.attr(this.name, this.value);
+	    		}
+			});
 
-		var numberOfFiles = $( ".multibrowse input:file" ).length;
-		var attributes = $( ".multibrowse input:file:first" ).prop( "attributes" );
-		$.each(attributes, function() {
-			if ( this.name == "id" || this.name == "name" ) {
-				$newFile.attr(this.name, (this.value) + ( numberOfFiles + 1 ));
-			} else {
-    			$newFile.attr(this.name, this.value);
-    		}
-		});
-
-		$( ".multibrowse input:file:last" ).after( $newFile );
+			$( ".multibrowse input:file:last" ).after( $newFile );
+		}
 	});
 
 }(jQuery));
