ef6839f3d82c5ea19ce3e5c23a83948c0d644499
David Morisset <dmorisset@gmail.com>
2015-11-16 20:31:45 +0000

    Adds functionality that renames the id and name attributes

--- multibrowse.js
+++ multibrowse.js
@@ -2,7 +2,18 @@
 
 	// Puts an event handler on every input of type file inside an element of class "multibrowse"
 	$(".multibrowse").delegate("input:file", 'change', function() {
-		var $newFile = $("<input type=\"file\">");
+		var $newFile = $("<input>");
+
+		var numberOfFiles = $( ".multibrowse input:file" ).length;
+		var attributes = $( ".multibrowse input:file:first" ).prop( "attributes" );
+		$.each(attributes, function() {
+			if ( this.name == "id" || this.name == "name" ) {
+				$newFile.attr(this.name, (this.value) + ( numberOfFiles + 1 ));
+			} else {
+    			$newFile.attr(this.name, this.value);
+    		}
+		});
+
 		$( ".multibrowse input:file:last" ).after( $newFile );
 	});
 
