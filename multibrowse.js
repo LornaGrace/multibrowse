a20b7eb4068b7e93ac9fd2d35e019d590f13df13
David Morisset <dmorisset@gmail.com>
2015-11-16 21:12:08 +0000

    Adds the function body and the settings object

--- multibrowse.js
+++ multibrowse.js
@@ -1,25 +1,36 @@
 (function( $ ) {
 
-	// Puts an event handler on every input of type file inside an element of class "multibrowse"
-	$(".multibrowse").delegate("input:file", 'change', function() {
-		
-		var $filesObject = $(".multibrowse input:file");
-		var numberOfInputs = $filesObject.length +1 ;
+	$multibrowse = $(".multibrowse");
 
-		if ( ($filesObject.filter(":last").val()) && (numberOfInputs <= 3) ) {
-			console.log( "NumberOfFiles is " + numberOfFiles );
-			var attributes = $filesObject.filter(":first").prop( "attributes" );
-			var $newFile = $("<input>");
-			$.each(attributes, function() {
-				if ( this.name == "id" || this.name == "name" ) {
-					$newFile.attr(this.name, (this.value) + ( numberOfFiles + 1 ));
-				} else {
-	    			$newFile.attr(this.name, this.value);
-	    		}
-			});
+	$.fn.addFileInput = function( options ){
 
-			$filesObject.filter(":last").after( $newFile );
-		}
-	});
+		var settings = $.extend({
+			maxNumberOfInputs: 3
+		}, options );
+
+		// Puts an event handler on every input of type file inside an element of class "multibrowse"
+		$multibrowse.delegate("input:file", 'change', function() {
+			
+			var $filesObject = $(".multibrowse input:file");
+			var numberOfInputs = $filesObject.length +1 ;
+
+			if ( ($filesObject.filter(":last").val()) && (numberOfInputs <= settings.maxNumberOfInputs) ) {
+				var attributes = $filesObject.filter(":first").prop( "attributes" );
+				var $newFile = $("<input>");
+				$.each(attributes, function() {
+					if ( this.name == "id" || this.name == "name" ) {
+						$newFile.attr(this.name, (this.value) + ( numberOfInputs + 1 ));
+					} else {
+		    			$newFile.attr(this.name, this.value);
+		    		}
+				});
+
+				$filesObject.filter(":last").after( $newFile );
+			}
+		});
+
+	}
+
+	$multibrowse.addFileInput();
 
 }(jQuery));
