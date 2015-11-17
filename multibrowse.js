8b210add9ecc7b343b6a0f8e023a7f672c5af66a
David Morisset <dmorisset@gmail.com>
2015-11-17 20:16:38 +0000

    Adds a writeMessage function and uses it to display a message if files are too big

--- multibrowse.js
+++ multibrowse.js
@@ -12,25 +12,32 @@
 	// Helper method that adds anothe file input after the last one 
 	$.fn.createFileInput = function( attributes ) {
 		var $newFile = $("<input>");
-		var $filesObject = $(".multibrowse input:file");
-
-		// Copies the attributes from first input, adds a suffix to "id" and "name"
+		// Copies the attributes adds a suffix to "id" and "name"
 		$.each(attributes, function() {
 			if ( this.name == "id" || this.name == "name" ) {
-				$newFile.attr(this.name, (this.value) + ( $filesObject.length +1 ));
+				$newFile.attr(this.name, (this.value) + ( $(".multibrowse input:file").length +1 ));
 			} else {
     			$newFile.attr(this.name, this.value);
     		}
 		});
-
-		$filesObject.filter(":last").after( $newFile ).after( "<br>" );
+		$(".multibrowse input:file:last").after( $newFile ).after( "<br>" );
 	}
+
+
+
+	$.fn.writeMessage = function( message ) {
+		var $message = $( "<p class=\"multiBrowseMessage\"></p>" );
+		$message.html( message );
+		$multibrowse.prepend( $message );
+	}
+
+
 
 	$.fn.addFileInput = function( options ){
 
 		// Default options
 		var settings = $.extend({
-			maxNumberOfInputs: 3,
+			maxNumberOfInputs: 5,
 			maxFileSize: 7000000,
 			maxTotalSize: 7000000
 		}, options );
@@ -38,31 +45,39 @@
 		// Puts an event handler on every input of type file inside an element of class "multibrowse"
 		$multibrowse.delegate("input:file", 'change', function() {
 			
-			var $filesObject = $(".multibrowse input:file");			
+			var $filesObject = $(".multibrowse input:file");
 			var attributes = $filesObject.filter(":first").prop( "attributes" );
 			var numberOfInputs = $filesObject.length;
+			
+			// Calculates the size of all files put together
 			var totalSize = 0;
-
 			$filesObject.each( function(index) {
-				totalSize += this.files[0].size;
+				// Skips the last input if empty (in case a file has been replaced in another input)
+				if ($filesObject[index].files[0] == undefined && index == numberOfInputs - 1) {
+					return true;
+				}
+				totalSize += $filesObject[index].files[0].size;
 			});
 
 			// Checks the file size, then the max file size
-			if ((  $filesObject[numberOfInputs -1].files[0].size > settings.maxFileSize) 
-				|| totalSize > settings.maxTotalSize ){
-				$filesObject.filter(":last").remove();
-				$multibrowse.createFileInput( attributes);
-				console.log( "Too big you idiot");
+			if ((  this.files[0].size > settings.maxFileSize) 
+			|| totalSize > settings.maxTotalSize ){
+				$message  = "Your files are too large";
+				$multibrowse.writeMessage( "Too Big you Idiot !!!" );
 			} 
+
 			// Adds another input of type file if the max number of inputs has not been reached
 			// AND the last file input is not empty
-			else if ( ($filesObject.filter(":last").val()) && (numberOfInputs <= settings.maxNumberOfInputs -1) ) {
+			if ( ($filesObject.filter(":last").val()) && (numberOfInputs <= settings.maxNumberOfInputs -1) ) {
 				$multibrowse.createFileInput( attributes);
 			}
-		});
 
+		});
+		
 	}
 
 	$multibrowse.addFileInput( userOptions );
 
 }(jQuery));
+
+
